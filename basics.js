0dff3bd46f5b64dc5e12ddae9973c0af63977ff5
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-01-20 18:07:13 -0800

    add first commit node.js

---
+++ basics.js
@@ -0,0 +1,13 @@
+// BASICS NODE.JS
+const name = 'mario';
+console.log(name);
+
+const name2 = 'Yoshi';
+console.log(name2);
+
+const greet = (name) => {
+  console.log(`hello, ${name}`);
+}
+
+greet('Chun-Li');
+greet('Link');
